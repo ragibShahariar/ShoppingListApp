74b3072ed230d967ca117ca3b092b3de1054f905
ragibShahariar <ragibshahariar07@gmail.com>
2024-08-20 14:29:48 +0600

    blank screen content added

--- lib/screens/grocery_list_screen.dart
+++ lib/screens/grocery_list_screen.dart
@@ -18,7 +18,7 @@ class _GroceryListScreenState extends State<GroceryListScreen> {
     Widget screenContent;
     if (items.isEmpty) {
       screenContent = const Center(
-        child: Text('No items added yet try adding one'),
+        child: Text('No items yet homie try add some...'),
       );
     } else {
       screenContent = ListView.builder(
